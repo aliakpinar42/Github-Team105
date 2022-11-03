dea17ce3577653e96d3df6bd4d565aacd9acc875
Ali AKPINAR <ali2.akpinar.42@gmail.com>
2022-11-03 12:16:44 +0100

    first commit

---
+++ src/ders0001/merhabadunya.java
@@ -0,0 +1,9 @@
+package ders0001;
+
+public class merhabadunya {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World Free Java");
+    }
+}
---
+++ src/ders9ifelse/ders09.java
@@ -0,0 +1,28 @@
+package ders9ifelse;
+
+import java.util.Scanner;
+
+public class ders09 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        "\nher kenardan sonra " +
+        "enter'a basin");
+
+
+
+        double kenar1=  scan.nextDouble();
+        double kenar2=  scan.nextDouble();
+    }   double kenar3=  scan.nextDouble();
+
+         if (kenar1==kenar2 && kenar2==kenar3){
+
+
+
+
+    }
+
+
+
+}
